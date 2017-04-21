9c0d70a7f4a0cee8ef60eb9bb48677f3d8ee7243
Vincent Chan <vincent.chan@biola.edu>
2017-04-21 11:14:37 -0700

    Add GET#new and POST#create tests for UsersController

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe "GET #new" do
+    it "renders signup page" do
+      get :new
+
+      expect(response).to render_template("new")
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST #create" do
+    it "saves new user to database" do
+      post :create, user: {username: "somebody", password: "password"}
+
+      expect(User.last.username).to eq("somebody")
+    end
+
+    it "validates presence of username and password" do
+      post :create, user: {username: "", password: ""}
+
+      expect(response).to render_template("new")
+      expect(flash[:errors]).to be_present
+    end
+
+    it "validates minimum length of password" do
+      post :create, user: {username: "Hank", password: "short"}
+
+      expect(response).to render_template("new")
+      expect(flash[:errors]).to be_present
+    end
+
+    it "redirects upon successful signup" do
+      post :create, user: {username: "Hank", password: "password"}
+
+      expect(response).to redirect_to(goals_url)
+    end
+
+    it "signs new user in" do
+      post :create, user: {username: "Hank", password: "password"}
+
+      expect(current_user.username).to be("Hank")
+    end
+  end
+end
